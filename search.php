43623d1d5dc0742b3318d819330c40bd0f18f0f3
Arnau Jane <penre12@gmail.com>
2019-03-24 15:03:35 +0100

    update search to avoid sql injections

--- search.php
+++ search.php
@@ -5,9 +5,11 @@
 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 <title>Search "<?php
-$q = $_GET["q"];
-echo $q;  
-
+//<Remove characters to protect from SQL injections>
+$searchVal = array("/", '"', "'","-",';','=');  
+$q = str_replace($searchVal, "", $_GET["q"]); 
+//</>
+	echo $q;
 ?>"</title>
 <div class="container">
     <br/>
