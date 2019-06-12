7f6d053bceca04a0d40dea5b02451fc8ff09b632
Arnau Jane <penre12@gmail.com>
2019-06-12 17:37:31 +0200

    Update account.php

--- account.php
+++ account.php
@@ -1,12 +1,5 @@
 <?php
-// Initialize the session
-session_start();
- 
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php?e=403");
-    exit;
-}
+require_once 'checkLogin.php';
 $f_username = $_SESSION["username"];
 ?>
 <!doctype html>
